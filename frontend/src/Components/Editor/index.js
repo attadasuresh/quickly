b2d300d80998c42055a6e168b98e29cd195c2401
attadasuresh <sureshattada12233@gmail.com>
2024-08-13 03:50:37 +0530

    dashboarworkd

--- frontend/src/Components/Editor/index.js
+++ frontend/src/Components/Editor/index.js
@@ -6,66 +6,88 @@ import { FaFileAudio } from "react-icons/fa";
 import { FaStar } from "react-icons/fa";
 import { IoMdSettings } from "react-icons/io";
 import { MdOutlineInsertPhoto } from "react-icons/md";
+import { MdOutlineStorage } from "react-icons/md";
+
+import { IoArrowBackCircleOutline } from "react-icons/io5";
+
 import "./index.css";
 
 const Editor = () => {
   const [sideBarButton, setSideBarButton] = useState("Home");
 
-
   const [searchValue, setSearchValue] = useState("");
 
-  const Home = ()=>{
-    return(
-      <h1>home</h1>
-    )
-  }
-  const Search = ()=>{
-    return(
-      <h1>Search</h1>
-    )
-  }
+  const [isOpen, setIsOpen] = useState(true);
+
+  const Home = () => {
+    return <h1>home</h1>;
+  };
+  const Search = () => {
+    return <h1>Search</h1>;
+  };
+  const Edit = () => {
+    return <h1>Edit</h1>;
+  };
   const sidebarData = [
     {
       name: "Home",
       icon: FaHome,
+      id: 1,
     },
     {
       name: "Search",
       icon: FaSearch,
+      id: 2,
     },
     {
       name: "Edit",
       icon: FaEdit,
+      id: 3,
     },
     {
       name: "Insert Image",
       icon: MdOutlineInsertPhoto,
+      id: 4,
     },
     {
       name: "Audio Transcribe",
       icon: FaFileAudio,
+      id: 5,
     },
     {
       name: "Review",
       icon: FaStar,
+      id: 6,
     },
   ];
-  const dashBoardView =()=>{
-    if(sideBarButton === "Home"){
-      return <Home/>
-    }else if(sideBarButton === "Search"){
-      return <Search/>
+  const dashBoardView = () => {
+    if (sideBarButton === "Home") {
+      return <Home />;
+    } else if (sideBarButton === "Search") {
+      return <Search />;
+    } else if (sideBarButton === "Edit") {
+      return <Edit />;
     }
-  }
-// 2 after filter the data 
-  const searchData = sidebarData.filter((each) =>
-    each.name.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase()) //searchValue Varible created for empty search show like input element
+  };
+  // 2 after filter the data
+  const searchData = sidebarData.filter(
+    (each) =>
+      each.name.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase()) //searchValue Varible created for empty search show like input element
   );
   return (
     <div className="editor-maincontainer">
-      <div className="editor-sidebar-full-container">
+      <div
+        className={`editor-sidebar-full-container ${isOpen && "open"}`}
+        style={{ position: isOpen ? "relative" : "absolute" }}
+      >
         <div className="editor-search-to-review-container">
-          <div className="editor-profil-card-container">
+          <div className="editor-profil-card-container-1">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="editor-profil-image1"
+            >
+              <IoArrowBackCircleOutline />
+            </button>
             <input
               type="search"
               placeholder="Search Tools Here"
@@ -76,7 +98,15 @@ const Editor = () => {
           {/* 1 first map the object data */}
           {searchData.map((each, index) => {
             return (
-              <button onClick={()=>setSideBarButton(each.name)} key={index} className="editor-profil-card-container">
+              <button
+                onClick={() => setSideBarButton(each.name)}
+                key={index}
+                className="editor-profil-card-container"
+                style={{
+                  backgroundColor:
+                    each.name === sideBarButton && "rgb(236, 180, 75)",
+                }}
+              >
                 <each.icon className="editor-profil-image" />
                 <h1 className="editor-profil-heading">{each.name}</h1>
               </button>
@@ -90,13 +120,17 @@ const Editor = () => {
         </div>
       </div>
 
-<div>
-  {
-    dashBoardView()
-  }
- 
-
-</div>
+      <div>
+        {!isOpen && (
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="editor-open-button"
+          >
+            <MdOutlineStorage />
+          </button>
+        )}
+        {dashBoardView()}
+      </div>
     </div>
   );
 };
